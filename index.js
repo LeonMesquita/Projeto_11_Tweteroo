9e77b3af4067d554539dd75d00bd4bd7bc0de5e1
LeonMesquita <leonmesquita16@gmail.com>
2022-06-16 16:40:15 -0300

    fazer login e postar um tweet

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-console.log("hello world");
+import express from 'express';
+import cors from 'cors';
+
+const server = express();
+server.use(cors());
+
+server.use(express.json());
+
+const usersList = [
+    {
+        username: 'bobesponja', 
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    },
+];
+
+const tweetsList = [
+    {
+       username: "bobesponja",
+       tweet: "eu amo o hub",
+       avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+
+    }
+];
+
+server.get('/', (request, response) => {
+    response.send(usersList);
+});
+
+
+server.post('/sign-up', (request, response) => {
+
+  console.log(request.body)
+
+    usersList.push(
+      request.body
+    );
+    response.send('ok');
+});
+
+
+server.post('/tweets', (request, response) => {
+    const username = request.body.username;
+    const user = usersList.find(newUser => newUser.username === username);
+    const tweetObj = {
+        username: username,
+        tweet: request.body.tweet,
+        avatar: user.avatar
+    }
+
+    tweetsList.push(
+        tweetObj
+    );
+    response.send('ok');
+
+});
+
+server.get('/tweets', (request, response) => {
+   
+    response.send(tweetsList);
+});
+
+server.listen(5000);
