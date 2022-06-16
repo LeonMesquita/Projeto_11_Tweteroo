e575c3a5cf10194e2dfb0df7b7b8c7a8cce976b0
LeonMesquita <leonmesquita16@gmail.com>
2022-06-16 17:00:53 -0300

    retorna os 10 últimos tweets postados

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const usersList = [
         username: 'bobesponja', 
 		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
     },
+    {
+        username: 'patrick', 
+		avatar: "https://zonacuriosa.com/wp-content/uploads/2020/11/curiosidades-incriveis-sobre-o-patrick-estrela.jpg",
+    },
+    {
+        username: 'molusco', 
+		avatar: "https://www.correiodopovo.com.br/image/policy:1.380556:1573842729/lulaa%20.jpg.jpg?a=1%3A1&$p$a=9519e63",
+    },
+    
 ];
 
 const tweetsList = [
@@ -18,8 +27,70 @@ const tweetsList = [
        username: "bobesponja",
        tweet: "eu amo o hub",
        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    },
+    {
+        username: "patrick",
+        tweet: "teste 1",
+        avatar: usersList[1].avatar,
+     },
+     {
+        username: "molusco",
+        tweet: "teste 2",
+        avatar: usersList[2].avatar,
+     },
+     {
+        username: "bobesponja",
+        tweet: "teste 3",
+        avatar: usersList[0].avatar,
+     },
+     {
+        username: "patrick",
+        tweet: "teste 4",
+        avatar: usersList[1].avatar,
+     },
+     {
+        username: "molusco",
+        tweet: "teste 5 ",
+        avatar: usersList[2].avatar,
+     },
+     {
+        username: "patrick",
+        tweet: "teste 6",
+        avatar: usersList[1].avatar,
+     },
+     {
+        username: "patrick",
+        tweet: "teste 7",
+        avatar: usersList[1].avatar,
+     },
+     {
+        username: "patrick",
+        tweet: "teste 8",
+        avatar: usersList[1].avatar,
+     },
+     {
+        username: "patrick",
+        tweet: "teste 9",
+        avatar: usersList[1].avatar,
+     },
+     {
+        username: "patrick",
+        tweet: "teste 10",
+        avatar: usersList[1].avatar,
+     },
 
-    }
+     {
+        username: "bobesponja",
+        tweet: "teste 11",
+        avatar: usersList[0].avatar,
+     },
+     {
+        username: "molusco",
+        tweet: "teste 12",
+        avatar: usersList[2].avatar,
+     },
+   
+             
 ];
 
 server.get('/', (request, response) => {
@@ -55,8 +126,15 @@ server.post('/tweets', (request, response) => {
 });
 
 server.get('/tweets', (request, response) => {
-   
-    response.send(tweetsList);
+    const lastTweets = [];
+    let qtdOfTweets = 0;
+    for (let count = tweetsList.length-1; count >= 0; count--){
+        lastTweets.push(tweetsList[count]);
+        qtdOfTweets++
+        if(qtdOfTweets === 10)
+            break;
+    }
+    response.send(lastTweets);
 });
 
 server.listen(5000);
